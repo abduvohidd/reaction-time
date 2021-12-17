d34e200d0446551fe1e2f203db725ec6c9c44105
Abduvohid <vohid4970@gmail.com>
2021-12-17 15:06:58 +0500

    my frist porject vue js

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button class="play" @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
+export default {
+  name: "App",
+  components: {
+    Block,
+    Results,
+  },
+
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+.play:hover {
+  cursor: pointer;
+  padding: 0.5rem 2rem;
+  background: rgb(165, 166, 168);
+  border: none;
+  border-radius: 5%;
+  transition: padding 0.5s ease-in-out;
+}
+.play {
+  transition: padding 0.5s ease-in-out;
+  font-size: 15px;
+}
+</style>
